e9e5def8ad0e1c92c43afcf6919e8bb30800da9a
Cvolton <cvolton@cvolton.eu>
2024-07-02 22:20:29 +0200

    add doxygen comments

--- include/EditorIDs.hpp
+++ include/EditorIDs.hpp
@@ -14,12 +14,59 @@
 #endif
 
 namespace EditorIDs {
+    /**
+     * @brief Get the editor ID of a level or list.
+     *
+     * @param level The level to get the ID of.
+     *
+     * @return The ID of the level, or m_levelID if the level is not editor.
+     */
     EDITORIDS_DLL int getID(GJGameLevel* level);
+
+    /**
+     * @brief Get the editor ID of a level or list.
+     *
+     * @param list The list to get the ID of.
+     *
+     * @return The ID of the level, or m_levelID if the level is not editor.
+     */
     EDITORIDS_DLL int getID(GJLevelList* level);
 
+    /**
+     * @brief Get the editor ID of a level or list.
+     *
+     * @param level The level to get the ID of.
+     * @param autoAssign Whether to assign an ID if it has not been assigned automatically
+     *
+     * @return The ID of the level, or m_levelID if the level is not editor.
+     */
     EDITORIDS_DLL int getID(GJGameLevel* level, bool autoAssign);
+
+    /**
+     * @brief Get the editor ID of a level or list.
+     *
+     * @param level The level to get the ID of.
+     * @param autoAssign Whether to assign an ID if it has not been assigned automatically
+     *
+     * @return The ID of the level, or m_levelID if the level is not editor.
+     */
     EDITORIDS_DLL int getID(GJLevelList* level, bool autoAssign);
 
+    /**
+     * @brief Get a level by its editor ID.
+     * 
+     * @param id The editor ID of the level.
+     *
+     * @return The level with the given ID, or nullptr if no level has that ID.
+     */
     EDITORIDS_DLL GJGameLevel* getLevelByID(int id);
+
+    /**
+     * @brief Get a list by its editor ID.
+     * 
+     * @param id The editor ID of the list.
+     *
+     * @return The list with the given ID, or nullptr if no list has that ID.
+     */
     EDITORIDS_DLL GJLevelList* getListByID(int id);
 }
